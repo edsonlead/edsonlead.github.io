f8b0b1268d267eb4c7a0d3dfc8308ab9b461dc8c
Edson Araújo <edsonlead@gmail.com>
2018-05-14 00:17:31 -0300

    add url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://edsonlead.com'
+SITEURL = 'https://edsonlead.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
